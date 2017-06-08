177919a23789037d1ba6dcdf2ad884732a219516
mdeloge <matteus.deloge2@gmail.com>
2017-06-08 22:13:46 +0200

    Update

--- AppEngineDashboard/app/login.py
+++ AppEngineDashboard/app/login.py
@@ -22,35 +22,16 @@ class LoginUser(CoreDashboardRequestHandler):
             if user_id == 1:
                 # In this case, account has not been confirmed.
                 correct = 1
-                print "Not confirmed"
                 self.render('login/login.html', correct=correct, email=email)
-                # status = 400
-                # json_response.update({
-                #     'loggedin': False,
-                #     'title': 'Account not confirmed',  # We don't want the whole key, only the ID.
-                #     'message': 'You have not yet confirmed your account. Check your inbox in order to get started.'
-                # })
             else:
                 # In this case, the user is correctly logged in.
                 self.send_cookie(name='User', value=user_id)
-                # html = self.jinja_environment.get_template('home/home.html').render()
-                # json_response.update({
-                #     'html': html
-                # })
                 self.redirect('/')
         else:
             # In this case, email or password are wrong.
             correct = 2
-            print "Wrong credentials"
             self.render('login/login.html', correct=correct, email=email)
-            # status = 400
-            # json_response.update({
-            #         'loggedin': False,
-            #         'title': 'Wrong credentials',  # We don't want the whole key, only the ID.
-            #         'message': 'Wrong email or password, please try again.'
-            #     })
 
-        # self.json_response(status_code=status, **json_response)
 
 
 class Logout(CoreDashboardRequestHandler):
